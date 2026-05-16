9d4288da0fff460b59cc0f48eb64f303c6c3726e
estheredi0406 <e.sessenou@alustudent.com>
2026-05-16 21:27:47 +0200

    Add props to MyComponent and counter to App

---
+++ my-first-react-app/src/App.tsx
@@ -0,0 +1,20 @@
+﻿import React, { useState } from 'react';
+import MyComponent from './MyComponent';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div className="App">
+      <MyComponent title="Hello from MyComponent!" />
+      <div className="counter">
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <span>{count}</span>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-first-react-app/src/MyComponent.tsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+import './MyComponent.css';
+
+interface Props {
+  title: string;
+}
+
+const MyComponent: React.FC<Props> = ({ title }) => {
+  return (
+    <div className="my-component">
+      <h1>{title}</h1>
+      <p>This is a paragraph of text within my component.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
